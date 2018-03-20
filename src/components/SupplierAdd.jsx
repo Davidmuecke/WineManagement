4d0acec08eb323c844fcbdb07eac99a29101f172
tinometzger <tino.metzger97@gmx.de>
2018-03-20 10:19:52 +0100

    Kunde und Winzer Seite überarbeitet

--- src/components/SupplierAdd.jsx
+++ src/components/SupplierAdd.jsx
@@ -4,7 +4,62 @@ import AdministrationBackup from "./AdministrationBackup";
 class SupplierAdd extends Component {
     render() {
         return (
-            <h1></h1>
+            <div className={"container"}>
+                <h2>Winzer hinzufügen</h2>
+                <form className={"form-horizontal"}>
+                    <div className={"row"}>
+                        <div className={"col-lg-4"}>
+                            <div className={"form-group row"}>
+                                <label className={"control-label col-sm-3 col-form-label"} for="sName">Name</label>
+                                <div className={"col-sm-9"}>
+                                    <input type="text" className="form-control sm-10" id="sName" />
+                                </div>
+                            </div>
+                            <div className={"form-group row"}>
+                                <label className={"control-label col-sm-3 col-form-label"} for="sRegion">Region</label>
+                                <div className={"col-sm-9"}>
+                                    <input type="text" className="form-control sm-10" id="sRegion" />
+                                </div>
+                            </div>
+                            <div className={"form-group row"}>
+                                <label className={"control-label col-sm-3 col-form-label"} for="sLand">Land</label>
+                                <div className={"col-sm-9"}>
+                                    <input type="text" className="form-control sm-10" id="sLand" />
+                                </div>
+
+                            </div>
+                        </div>
+                        <div className={"col-lg-4"}>
+                            <div className={"form-group row"}>
+                                <label className={"control-label col-sm-3 col-form-label"} for="sStrasse">Straße</label>
+                                <div className={"col-sm-9"}>
+                                    <input type="text" className="form-control sm-10" id="sStrasse" />
+                                </div>
+                            </div>
+                            <div className={"form-group row"}>
+                                <label className={"control-label col-sm-3 col-form-label"} for="sOrt">Ort</label>
+                                <div className={"col-sm-9"}>
+                                    <input type="text" className="form-control sm-10" id="sOrt" />
+                                </div>
+                            </div>
+                            <div className={"form-group row"}>
+                                <label className={"control-label col-sm-3 col-form-label"} for="sPLZ">PLZ</label>
+                                <div className={"col-sm-9"}>
+                                    <input type="text" className="form-control sm-10" id="sPLZ" />
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+                    <div className={"row"}>
+
+                        <div className={"col-lg-8"}>
+                            <button type="submit" className="btn btn-secondary btn-sup">Abbrechen</button>
+                            <button type="submit" className="btn btn-primary btn-sup" >Speichern</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
         )
     }
 }
--- src/components/SupplierOverview.jsx
+++ src/components/SupplierOverview.jsx
@@ -4,57 +4,7 @@ import AdministrationBackup from "./AdministrationBackup";
 class SupplierOverview extends Component {
     render() {
         return (
-            <div className={"container"}>
-            <h2>Winzer hinzufügen</h2>
-            <form className={"form-horizontal"}>
-                <div className={"row"}>
-                    <div className={"col-lg-4"}>
-                        <div className={"form-group row"}>
-                            <label className={"control-label col-sm-3 col-form-label"} for="sName">Name</label>
-                            <div className={"col-sm-9"}>
-                                <input type="text" className="form-control sm-10" id="sName" />
-                            </div>
-                        </div>
-                        <div className={"form-group row"}>
-                            <label className={"control-label col-sm-3 col-form-label"} for="sRegion">Region</label>
-                            <div className={"col-sm-9"}>
-                                <input type="text" className="form-control sm-10" id="sRegion" />
-                            </div>
-                        </div>
-                        <div className={"form-group row"}>
-                            <label className={"control-label col-sm-3 col-form-label"} for="sLand">Land</label>
-                            <div className={"col-sm-9"}>
-                                <input type="text" className="form-control sm-10" id="sLand" />
-                            </div>
-
-                        </div>
-                    </div>
-                    <div className={"col-lg-4"}>
-                        <div className={"form-group row"}>
-                            <label className={"control-label col-sm-3 col-form-label"} for="sStrasse">Straße</label>
-                            <div className={"col-sm-9"}>
-                                <input type="text" className="form-control sm-10" id="sStrasse" />
-                            </div>
-                        </div>
-                        <div className={"form-group row"}>
-                            <label className={"control-label col-sm-3 col-form-label"} for="sOrt">Ort</label>
-                            <div className={"col-sm-9"}>
-                                <input type="text" className="form-control sm-10" id="sOrt" />
-                            </div>
-                        </div>
-                        <div className={"form-group row"}>
-                            <label className={"control-label col-sm-3 col-form-label"} for="sPLZ">PLZ</label>
-                            <div className={"col-sm-9"}>
-                                <input type="text" className="form-control sm-10" id="sPLZ" />
-                            </div>
-
-                        </div>
-                    </div>
-                </div>
-                <button type="submit" className="btn btn-primary">Abbrechen</button>
-                <button type="submit" className="btn btn-primary">Speichern</button>
-            </form>
-        </div>
+            <h1></h1>
         )
     }
 }
