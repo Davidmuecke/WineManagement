d35a854a593f5fc07b3425041486d0d242c5cd14
JOW <john.geo16@gmail.com>
2018-03-20 15:49:00 +0100

    Fügt Sicht "Neue Bestellung" hinzu.

--- src/components/OrderOverview.jsx
+++ src/components/OrderOverview.jsx
@@ -3,11 +3,20 @@ import AdministrationBackup from "./AdministrationBackup";
 
 class OrderOverview extends Component {
     render() {
+
         return (
             <div className={"container"}>
                 <h2>Übersicht Bestellungen</h2>
                 <form className={"form-horizontal"}>
-                    <input type="text" placeholder={"Bestellung suchen"} className="form-control sm-10" id="wName" />
+                        <div class="input-group col-md-20">
+                            <input type="text" placeholder="Bestellung suchen" className="form-control sm-10" id="wName" />
+                            <span class="input-group-btn">
+                                    <button class="btn" type="button">
+                                        <i class="fas fa-search"></i>
+                                    </button>
+                                </span>
+                        </div>
+
                     <div class="table-responsive">
                     <table className={"table table-striped"}>
                         <thead>
@@ -19,18 +28,18 @@ class OrderOverview extends Component {
                             <th>Jahrgang</th>
                             <th>Einzelpreis</th>
                             <th>Gesamtpreis</th>
-                            <th>delete</th>
+                            <th><i class="fas fa-trash-alt"></i></th>
                         </tr>
                         </thead>
                         <tbody>
                         <tr>
-                            <td>13571275</td><td>Max Mustermann</td><td>Rotwein</td><td>Werner Winzer</td><td>1960</td><td>5,00€</td><td>20,00€</td><td><a href={"link.htm"}>delete</a></td>
+                            <td>13571275</td><td>Max Mustermann</td><td>Rotwein</td><td>Werner Winzer</td><td>1960</td><td>5,00€</td><td>20,00€</td><td><a href={"link.htm"}><i class="fas fa-trash-alt"></i></a></td>
                         </tr>
                         <tr>
-                            <td>13571275</td><td>Max Mustermann</td><td>Rotwein</td><td>Werner Winzer</td><td>1960</td><td>5,00€</td><td>20,00€</td><td><a href={"link.htm"}>delete</a></td>
+                            <td>13571276</td><td>Max Mustermann</td><td>Rotwein</td><td>Werner Winzer</td><td>1960</td><td>5,00€</td><td>20,00€</td><td><a href={"link.htm"}><i class="fas fa-trash-alt"></i></a></td>
                         </tr>
                         <tr>
-                            <td>13571275</td><td>Max Mustermann</td><td>Rotwein</td><td>Werner Winzer</td><td>1960</td><td>5,00€</td><td>20,00€</td><td><a href={"link.htm"}>delete</a></td>
+                            <td>13571277</td><td>Max Mustermann</td><td>Rotwein</td><td>Werner Winzer</td><td>1960</td><td>5,00€</td><td>20,00€</td><td><a href={"link.htm"}><i class="fas fa-trash-alt"></i></a></td>
                         </tr>
                         </tbody>
                     </table>
