1086d0c87fe8afa0d1086c2c95bb87d206a66050
JOW <john.geo16@gmail.com>
2018-03-20 13:53:26 +0100

    Fügt OrderAdd und OrderOverview hinzu.

--- src/components/OrderAdd.jsx
+++ src/components/OrderAdd.jsx
@@ -3,9 +3,65 @@ import AdministrationBackup from "./AdministrationBackup";
 
 class OrderAdd extends Component {
     render() {
-        return {
-
-        }
+        return (
+            <div className={"container"}>
+                <h2>Neue Bestellung</h2>
+                <form className={"form-horizontal"}>
+                    <div className={"row"}>
+                        <div className={"col-lg-4"}>
+                            <div className="form-group">
+                                <table className={"table sm-2"}>
+                                    <tr>
+                                        <td>test</td><td>test2</td><td>test3</td>
+                                    </tr>
+                                    <tr>
+                                        <td>test</td><td>test2</td><td>test3</td>
+                                    </tr>
+                                </table>
+                            </div>
+                            <div className="form-group">
+                                <label for="wJahrgang">Jahrgang</label>
+                                <input type="number" className="form-control" id="wJahrgang" />
+                            </div>
+                            <div className="form-group">
+                                <label for="wMenge">Menge</label>
+                                <input type="number" className="form-control" id="wMenge" />
+                            </div>
+                        </div>
+                        <div className={"col-lg-4"}>
+                            <div className={"form-group row"}>
+                                <label className={"control-label col-sm-3 col-form-label"} for="wName">Name</label>
+                                <div className={"col-sm-9"}>
+                                    <input type="text" className="form-control sm-10" id="wName" />
+                                </div>
+                            </div>
+                            <div className={"form-group row"}>
+                                <label className={"control-label col-sm-3 col-form-label"} for="wJahrgang">Jahrgang</label>
+                                <div className={"col-sm-9"}>
+                                    <input type="number" className="form-control sm-10" id="wJahrgang" />
+                                </div>
+                            </div>
+                            <div className={"form-group row"}>
+                                <label className={"control-label col-sm-3 col-form-label"} for="wMenge">Liefermenge</label>
+                                <div className={"col-sm-9"}>
+                                    <input type="number" className="form-control sm-10" id="wMenge" />
+                                </div>
+                            </div>
+                            <div className={"form-group row"}>
+                                <label className={"control-label col-sm-3 col-form-label"} for="wLieferdatum">Lieferdatum</label>
+                                <div className={"col-sm-9"}>
+                                    <input type="date" className="form-control sm-10" id="wLieferdatum" />
+                                </div>
+                            </div>
+                        </div>
+                        <div className={"col-lg-4"}>
+                            drei
+                        </div>
+                    </div>
+                    <button type="submit" className="btn btn-primary">Submit</button>
+                </form>
+            </div>
+        );
     }
 }
 
--- src/components/OrderOverview.jsx
+++ src/components/OrderOverview.jsx
@@ -3,9 +3,44 @@ import AdministrationBackup from "./AdministrationBackup";
 
 class OrderOverview extends Component {
     render() {
-        return {
+        return (
+            <div className={"container"}>
+                <h2>Übersicht Bestellungen</h2>
+                <form className={"form-horizontal"}>
+                    <input type="text" placeholder={"Bestellung suchen"} className="form-control sm-10" id="wName" />
+                    <div class="table-responsive">
+                    <table className={"table table-striped"}>
+                        <thead>
+                        <tr>
+                            <th>Nummer</th>
+                            <th>Kunde</th>
+                            <th>Weinsorte</th>
+                            <th>Winzer</th>
+                            <th>Jahrgang</th>
+                            <th>Einzelpreis</th>
+                            <th>Gesamtpreis</th>
+                            <th>delete</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <tr>
+                            <td>13571275</td><td>Max Mustermann</td><td>Rotwein</td><td>Werner Winzer</td><td>1960</td><td>5,00€</td><td>20,00€</td><td><a href={"link.htm"}>delete</a></td>
+                        </tr>
+                        <tr>
+                            <td>13571275</td><td>Max Mustermann</td><td>Rotwein</td><td>Werner Winzer</td><td>1960</td><td>5,00€</td><td>20,00€</td><td><a href={"link.htm"}>delete</a></td>
+                        </tr>
+                        <tr>
+                            <td>13571275</td><td>Max Mustermann</td><td>Rotwein</td><td>Werner Winzer</td><td>1960</td><td>5,00€</td><td>20,00€</td><td><a href={"link.htm"}>delete</a></td>
+                        </tr>
+                        </tbody>
+                    </table>
+                    </div>
 
-        }
+
+                    <button className="btn btn-primary float-right" >Neue Bestellung</button>
+                </form>
+            </div>
+        );
     }
 }
 
